bc20e5a472205b6af0276c9bccf1b08fa8d9643c
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-26 05:31:55 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class Patient 
+  @@all = []
+  attr_accessor :name, :appointments
+  
+  def initialize(name)
+    @name = name 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def new_appointment(date, doctor)
+    new_app = Appointment.new(self, date, doctor)
+    Appointment.all << new_app 
+    new_app
+  end
+  
+  def appointments
+    Appointment.all.select do |appt|
+      appt.patient == self
+    end
+  end 
+  
+  def doctors 
+    appointments.map {|appt| appt.doctor}
+    binding.pry
+  end 
+  
+end 
