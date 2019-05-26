19c849f0c6d152b6571c20f405f4f3329b0a4107
Jenna Ransom <jenna.ransom@gmail.com>
2019-05-26 05:36:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,7 +13,7 @@ class Patient
   end 
   
   def new_appointment(date, doctor)
-    new_app = Appointment.new(self, date, doctor)
+    new_app = Appointment.new(self, doctor, date)
     Appointment.all << new_app 
     new_app
     binding.pry
